2e94a067e722b22983a27cac67998dae61180697
eL <elvisiondragon@gmail.com>
2026-01-27 08:06:45 +0700

    NAH CHANG2E BAD FBCLID

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,14 @@ import IntroLanding from './intro.tsx'
 import DisplayPage from './display.tsx'
 import UangPanasLanding from './id_ebook/ebook_uangpanas.tsx'
 import EbookFeminineLanding from './id_ebook/ebook_feminine.tsx'
+import EbookAdhdLanding from './id_ebook/ebook_adhd.tsx'
+import ArifEbookLanding from './id_ebook/ebook_arif.tsx'
+import EbookElvisionPaymentPage from './id_ebook/ebook_elvision.tsx'
+import EbookGriefLanding from './id_ebook/ebook_grief.tsx'
+import DietPaymentPage from './id_ebook/ebook_langsing.tsx'
+import EbookPercayaDiriLP from './id_ebook/ebook_percayadiri.tsx'
+import EbookTrackerLanding from './id_ebook/ebook_tracker.tsx'
+import ELVision15K from './id_ebook/vip_15jt.tsx'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { SpeedInsights } from "@vercel/speed-insights/react"
@@ -14,6 +22,14 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Route path="/intro" element={<IntroLanding />} />
       <Route path="/ebook_uangpanas" element={<UangPanasLanding />} />
       <Route path="/ebook_feminine" element={<EbookFeminineLanding />} />
+      <Route path="/ebook_adhd" element={<EbookAdhdLanding />} />
+      <Route path="/ebook_arif" element={<ArifEbookLanding />} />
+      <Route path="/ebook_elvision" element={<EbookElvisionPaymentPage />} />
+      <Route path="/ebook_grief" element={<EbookGriefLanding />} />
+      <Route path="/ebook_langsing" element={<DietPaymentPage />} />
+      <Route path="/ebook_percayadiri" element={<EbookPercayaDiriLP />} />
+      <Route path="/ebook_tracker" element={<EbookTrackerLanding />} />
+      <Route path="/vip_15jt" element={<ELVision15K />} />
     </Routes>
     <SpeedInsights />
   </BrowserRouter>,
